423e685454b8d32ba02a78da43609999cd48497c
Вячеслав Передеренко <v.perederenko@balance-pl.ru>
2024-04-28 19:27:35 +0300

    Fix errors

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,69 +2,69 @@
   description = "MediaCenter NixOS Configuration";
 
   inputs = {
-      nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
 
-      home-manager = {
-        url = "github:nix-community/home-manager/release-23.11";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
     in {
-    nixosConfigurations =  {
-      kodi = nixpkgs.lib.nixosSystem;
-
-      modules = [
-        ./configuration.nix
-        ./hardware-configuration.nix
-        # ./nvidia.nix
-        # ./disable-nvidia.nix
-        ./opengl.nix
-        # ./fingerprint-scanner.nix
-        # I don't want yobikey
-        #./yubikey.nix
-        ./sound.nix
-        ./usb.nix
-        ./time.nix
-        ./swap.nix
-        ./bootloader.nix
-        ./nix-settings.nix
-        ./nixpkgs.nix
-        ./gc.nix
-        ./auto-upgrade.nix
-        ./linux-kernel.nix
-        ./screen.nix
-        ./display-manager.nix
-        ./theme.nix
-        ./internationalisation.nix
-        ./fonts.nix
-        ./security-services.nix
-        ./services.nix
-        # ./printing.nix
-        # ./gnome.nix
-        ./hyprland.nix
-        ./environment-variables.nix
-        ./bluetooth.nix
-        ./networking.nix
-        # ./mac-randomize.nix
-        ./open-ssh.nix
-        ./firewall.nix
-        ./dns.nix
-        # ./vpn.nix
-        ./users.nix
-        # ./virtualisation.nix
-        ./programming-languages.nix
-        # ./lsp.nix
-        # ./rust.nix
-        # ./wasm.nix
-        ./info-fetchers.nix
-        ./utils.nix
-        ./terminal-utils.nix
-        # ./work.nix
-      ];
+      nixosConfigurations =  {
+        kodi = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./configuration.nix
+            ./hardware-configuration.nix
+            # ./nvidia.nix
+            # ./disable-nvidia.nix
+            ./opengl.nix
+            # ./fingerprint-scanner.nix
+            # I don't want yobikey
+            #./yubikey.nix
+            ./sound.nix
+            ./usb.nix
+            ./time.nix
+            ./swap.nix
+            ./bootloader.nix
+            ./nix-settings.nix
+            ./nixpkgs.nix
+            ./gc.nix
+            ./auto-upgrade.nix
+            ./linux-kernel.nix
+            ./screen.nix
+            ./display-manager.nix
+            ./theme.nix
+            ./internationalisation.nix
+            ./fonts.nix
+            ./security-services.nix
+            ./services.nix
+            # ./printing.nix
+            # ./gnome.nix
+            ./hyprland.nix
+            ./environment-variables.nix
+            ./bluetooth.nix
+            ./networking.nix
+            # ./mac-randomize.nix
+            ./open-ssh.nix
+            ./firewall.nix
+            ./dns.nix
+            # ./vpn.nix
+            ./users.nix
+            # ./virtualisation.nix
+            ./programming-languages.nix
+            # ./lsp.nix
+            # ./rust.nix
+            # ./wasm.nix
+            ./info-fetchers.nix
+            ./utils.nix
+            ./terminal-utils.nix
+            # ./work.nix
+          ];
+      };
     };
   };
 }
