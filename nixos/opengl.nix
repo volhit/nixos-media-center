a39032654148eb8bb8e78c40ae660a38cd7a2b1b
Вячеслав Передеренко <v.perederenko@balance-pl.ru>
2024-04-28 19:36:11 +0300

    OpenGl fixes

--- nixos/opengl.nix
+++ nixos/opengl.nix
@@ -9,7 +9,7 @@
   hardware.opengl = {
     enable = true;
     driSupport = true;
-    driSupport32Bit = true;
+    # driSupport32Bit = true; # FIXIT in a real hardware
     extraPackages = with pkgs; [
       intel-compute-runtime
       intel-media-driver    # LIBVA_DRIVER_NAME=iHD
