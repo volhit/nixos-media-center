cb7f8d72b5d780a870c0d176e1b325612962ac13
Вячеслав Передеренко <v.perederenko@balance-pl.ru>
2024-04-28 23:21:38 +0300

    Firewall settings

--- nixos/firewall.nix
+++ nixos/firewall.nix
@@ -3,7 +3,8 @@
 {
   # Open ports in the firewall.
   networking.firewall.enable = true;
-  # networking.firewall.allowedTCPPorts = [ 3000 ];
+  # kodi remote control
+  networking.firewall.allowedTCPPorts = [ 8080 ];
   # networking.firewall.allowedUDPPorts = [ 3000 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
